fb36838ea181ae12605f581a926be068f4e6456e
Hector Cortes <hector.cortes@outlook.com>
2020-11-13 01:08:03 -0800

    Extend comment lines

--- iam.tf
+++ iam.tf
@@ -1,16 +1,16 @@
-#################################################
+###################################################
 # Local Variables
-#################################################
+###################################################
 locals {
   iam_groups = yamldecode(file("${path.module}/config_files/iam.yaml"))["groups"]
   iam_policies = yamldecode(file("${path.module}/config_files/iam.yaml"))["policies"]
 }
 
-#################################################
+###################################################
 # IAM Users
 # Creates an IAM User for each unique entry in
 # local.iam_groups.members
-#################################################
+###################################################
 resource "aws_iam_user" "users" {
   for_each = toset(distinct(flatten([
     for iam_group in local.iam_groups:
@@ -22,11 +22,11 @@ resource "aws_iam_user" "users" {
   force_destroy = true
 }
 
-#################################################
+###################################################
 # IAM Groups
 # Creates an IAM Group for each key in
 # local.iam_groups
-#################################################
+###################################################
 resource "aws_iam_group" "groups" {
   for_each = local.iam_groups
 
@@ -34,11 +34,11 @@ resource "aws_iam_group" "groups" {
   path = "/user_groups/"
 }
 
-#################################################
-# IAM Group Membership
+###################################################
+# IAM Group Memberships
 # Assigns IAM users to IAM groups, based off of
 # local.iam_groups.members
-#################################################
+###################################################
 resource "aws_iam_user_group_membership" "group_members" {
   for_each = transpose({
     for iam_group in local.iam_groups :
