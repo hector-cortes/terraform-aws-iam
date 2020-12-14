f9c92c6042703924daec639f6749edb753c1cde6
Hector Cortes <hector.cortes@outlook.com>
2020-12-13 21:36:03 -0800

    Refactor scripts folder

--- scripts/generate_iam_template.py
+++ scripts/generate_iam_template.py
@@ -3,50 +3,57 @@ import yaml
 
 from jinja2 import FileSystemLoader, Environment
 
-CONFIG = "./iam_struct.yaml"
-TEMPLATE = "./iam_template.jinja"
+CONFIG = "config/config.yaml"
+IAM_USERS_TEMPLATE = "templates/iam_users.jinja"
+IAM_USERS_AUTOGEN_CONFIG = "terraform/terraform_autogen_config.yaml"
 
+# Load Jinja template
 template_loader = FileSystemLoader(searchpath="./")
 template_env = Environment(loader=template_loader)
-template = template_env.get_template(TEMPLATE)
+iam_users_template = template_env.get_template(IAM_USERS_TEMPLATE)
 
-
+# Load IAM config file
 with open(CONFIG, "r") as raw_config:
     config = yaml.safe_load(raw_config)
 
+# Generate IAM Users
+iam_users = []
+for group, group_attrs in config["iam_groups"].items():
+    iam_users.extend(group_attrs["members"])
+iam_users = sorted(set(iam_users))
+
+# Generate IAM Groups
 iam_groups = config["iam_groups"]
-iam_policies = config["iam_policies"]
-aws_accounts = config["accounts"]
 
-users = []
-for group, group_attrs in iam_groups.items():
-    users.extend(group_attrs["members"])
-users = sorted(set(users))
-
-policies = {}
-for policy, policy_attrs in iam_policies.items():
+# Generate IAM Policies
+assume_role_policies = {}
+for policy, policy_attrs in config["assume_role_policies"].items():
+    assume_role_policies[policy] = []
     for account_group in policy_attrs["accounts"]:
-        role_arns = [
-            f"arn:aws:iam::{account_id}:role/{role}"
-            for role in policy_attrs["roles"]
-            for account_id in aws_accounts[account_group]
-        ]
-    policies.update({policy: role_arns})
+        assume_role_policies[policy].extend(
+            [
+                f"arn:aws:iam::{account_id}:role/{role}"
+                for role in policy_attrs["roles"]
+                for account_id in config["accounts"][account_group]
+            ]
+        )
 
-policy_attachments = {}
-for group, group_attrs in iam_groups.items():
-    policy_attachments.update(
+# Generate IAM Policy Attachments
+assume_role_policy_attachments = {}
+for group, group_attrs in config["iam_groups"].items():
+    assume_role_policy_attachments.update(
         {
             f"{group}_{policy}": {"group": group, "policy": policy}
             for policy in group_attrs["policies"]
         }
     )
 
-with open("iam_temp.yaml", "w") as temp:
-    test = template.render(
+# Update terraform_autogen_config.yaml with new template
+with open(IAM_USERS_AUTOGEN_CONFIG, "w") as iam_config:
+    rendered_iam_template = template.render(
         groups=iam_groups,
         users=users,
         policies=policies,
         policy_attachments=policy_attachments,
     )
-    temp.write(test)
+    iam_config.write(rendered_iam_template)
--- scripts/terraform-fmt.sh
+++ scripts/terraform-fmt.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
-
-# 
+#
+# Edits each commit made in the current branch by running Terraform fmt
 git_rebase_in_progress() {
     [[ -d "$(git rev-parse --git-path rebase-merge)" || -d "$(git rev-parse --git-path rebase-apply)" ]]
 }
 
+# Runs git rebase -i, starting from the first commit made on the active branch
 current_branch=$(git rev-parse --abbrev-ref HEAD)
-first_branch_commit=$(git log main..$current_branch --format=format:%H | tail -1)
-GIT_SEQUENCE_EDITOR="sed -i -re 's/^pick /e /'" git rebase -i $first_branch_commit
+first_branch_commit=$(git log main.."$current_branch" --format=format:%H | tail -1)
+GIT_SEQUENCE_EDITOR="sed -i -re 's/^pick /e /'" git rebase -i "$first_branch_commit"
 
+# Runs terraform fmt on each commit in the current branch
 while git_rebase_in_progress
 do
-    echo "Run fmt, rebase"
     edited_files=$(terraform fmt -recursive)
-    git add $edited_files
+    git add "$edited_files"
     git commit --amend --no-edit
     GIT_EDITOR=true git rebase --continue
 done
